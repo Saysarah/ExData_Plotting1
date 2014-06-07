6d29a8fc1e19e707a4953f0e08d5206d1109850b
SaySarah <Saysarah@gmail.com>
2014-06-07 01:52:49 -0400

    updated Plot3.R removed xlab

--- plot3.R
+++ plot3.R
@@ -8,7 +8,7 @@ y <- powerData$Sub_metering_1
 y2 <- powerData$Sub_metering_2
 y3 <- powerData$Sub_metering_3
 
-plot(x,y, type = "n", ylab = "Energy sub metering") ## empty plot 
+plot(x,y, type = "n", xlab = "", ylab = "Energy sub metering") ## empty plot 
 points(x,y, type = "l", col = "black") ## add in data one colour at a time
 points(x,y2, type = "l", col = "red")
 points(x,y3, type = "l", col = "blue")
