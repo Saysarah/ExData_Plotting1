86bb214eff93c5f3a925ebd3e4d5ad58d40835d5
SaySarah <Saysarah@gmail.com>
2014-06-08 00:19:57 -0400

    reformated R code for plot4.R

--- plot4.R
+++ plot4.R
@@ -1,6 +1,11 @@
 library(datasets)
 ## File contains 9 col, and 2075259 rows, only read desired data
-powerData <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), nrows = 2880, skip = 66636, na.strings=c("?","NA"))
+powerData <- read.table("household_power_consumption.txt",
+                        header = TRUE, sep = ";", col.names = c("Date", "Time",
+                        "Global_active_power", "Global_reactive_power", "Voltage",
+                        "Global_intensity", "Sub_metering_1", "Sub_metering_2",
+                        "Sub_metering_3"), nrows = 2880, skip = 66636,
+                        na.strings=c("?","NA"))
 
 x <- as.POSIXct( strptime(paste(powerData$Date, powerData$Time), "%d/%m/%Y %H:%M:%S"))
 png(filename = "plot4.png", width = 480, height = 480, units = "px") ## Opens PNG device
@@ -23,7 +28,8 @@ plot(x,y, type = "n", xlab = "", ylab = "Energy sub metering") ## empty plot
 points(x,y, type = "l", col = "black") ## add in data one colour at a time
 points(x,y2, type = "l", col = "red")
 points(x,y3, type = "l", col = "blue")
-legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col = c("black", "red","blue"), lwd = 1, bty = "n")
+legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       col = c("black", "red","blue"), lwd = 1, bty = "n")
 
 z2 <- powerData$Global_reactive_power
 plot(x,z2, type = "l", xlab = "datetime",ylab = "Global_reactive_power")
